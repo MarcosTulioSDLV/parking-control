163cc750df0f386f428da68f1a66004cfe806d89
MarcosTulioSDLV <mtsotodelavega@gmail.com>
2023-12-28 10:44:39 -0500

    spring boot parking control temp branch

--- src/main/java/com/api/parkingcontrol/config/SwaggerConfig.java
+++ src/main/java/com/api/parkingcontrol/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Parking Control API Rest")
+                .title("Rest API Parking Control")
                 .description("This API is for managing Parking Spots in a Condominium, providing all CRUD (Create, read, update and delete) operations.")
                 .version("1.0")
                 .contact(new Contact("Marcos Soto", "https://ar.linkedin.com/in/marcos-tulio-soto-de-la-vega-8a6b9668", "mtsotodelavega@gmail.com"))
